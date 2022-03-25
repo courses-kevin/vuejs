2e5127f49713e1d2e442c7a05c04cf99df81182d
RIOU Kevin <kevin@riou.pro>
2022-03-25 15:18:04 +0100

    ✨feat(Component): presentation Mode

--- components/PresentationMode.jsx
+++ components/PresentationMode.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
-import { MODES } from '../constants/modes'
+import React from "react";
+import styled from "styled-components";
+import { MODES } from "../constants/modes";
 
 const PresentationFrame = styled.div`
   display: flex;
   padding: 3rem;
   max-height: 100vh;
-`
+  background-color: #f0f0f0;
+  height: 100vh;
+`;
 
 const SlideWindow = styled.div`
   width: 65%;
   overflow-y: scroll;
+  aspect-ratio: 16 / 9;
 
   & > div {
     padding: 1rem;
@@ -18,11 +21,16 @@ const SlideWindow = styled.div`
   }
 
   #slide {
-    border: 1px solid #fff;
-    align-items:flex-start;
-    height:auto;
+    align-items: flex-start;
+    height: auto;
+    aspect-ratio: 16 / 10;
+    max-width: calc(60vw - 80px);
+    border-radius: 2px;
+    background: linear-gradient(145deg, #f8f8f8, #ffffff);
+    box-shadow: 13px 13px 26px #b6b6b6, -13px -13px 26px #ffffff;
+    margin: 40px;
   }
-`
+`;
 
 const Sidebar = styled.div`
   width: 35%;
@@ -30,20 +38,23 @@ const Sidebar = styled.div`
   & > div {
     padding: 1rem;
   }
-`
+`;
 
 const SpeakerNotesWindow = styled.div`
-  width: 100%;
-  height: 50vh;
+  width: calc(100% - 80px);
+  height: 60vh;
   overflow-y: scroll;
-  border: 1px solid #fff;
+  border-radius: 10px;
+  background: linear-gradient(145deg, #f8f8f8, #ffffff);
+  box-shadow: 7px 7px 14px #b6b6b6, -7px -7px 14px #ffffff;
+  margin: 40px;
 
-  font-size: 16px;
+  font-size: 26px;
 
   & > div {
     padding: 1rem;
   }
-`
+`;
 
 export default function PresentationMode({
   mode,
@@ -66,7 +77,7 @@ export default function PresentationMode({
           </div>
         </Sidebar>
       </PresentationFrame>
-    )
+    );
   }
-  return children
+  return children;
 }
--- components/SpeakerNotes.jsx
+++ components/SpeakerNotes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function SpeakerNotes({ children }) {
-  return <div style={{ backgroundColor: "black" }}>{children}</div>;
+  return <div>{children}</div>;
 }
